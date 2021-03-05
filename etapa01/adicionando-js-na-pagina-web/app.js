fd9f6e384b53370c46ef1f057f9d80e5393b2727
Galileu Rodrigues <rlgalileu@gmail.com>
2021-03-05 10:37:49 -0300

    Exercicio 01 - Finalizado

--- etapa01/adicionando-js-na-pagina-web/app.js
+++ etapa01/adicionando-js-na-pagina-web/app.js
@@ -1,1 +1,19 @@
-alert('JS is Awesome =)')
+// alert('JS is Awesome =)')
+
+// console.log('Testanto DevTools')
+
+// Criando variáveis
+let age = 30
+let currentyear = 2021
+
+console.log(age, currentyear)
+
+age = 31
+console.log(age)
+
+
+// Criando uma constante
+const points = 100
+// points = 101  /* TypeError: Assignment to constant variable. */
+
+console.log(points)
