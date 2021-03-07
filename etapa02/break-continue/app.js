96199f727a2dc3838b8639cb98c2fbe806c0c100
Galileu Rodrigues <rlgalileu@gmail.com>
2021-03-07 20:48:49 -0300

    Aulas (Operador Not, Condicional switch e break-continue) Finalizadas

---
+++ etapa02/break-continue/app.js
@@ -0,0 +1,18 @@
+const scores = [50, 25, 0, 30, 100, 20, 10]
+
+for (let i = 0; i < scores.length; i++) {
+  if (scores[i] === 0) {
+    continue
+  }
+
+  console.log(`sua pontuação: ${scores[i]}`)
+  
+  if (scores[i] === 100) {
+    console.log('Parabéns, você atingiu a pontuação máxima.')
+    
+    break
+  }
+}
+
+
+
---
+++ etapa02/operador-not/app.js
@@ -0,0 +1,7 @@
+// O operador lógico not (!)
+
+let isUserLoggedIn = false
+
+if (!isUserLoggedIn) {
+  console.log('Você precisa fazer login para continuar')
+}
