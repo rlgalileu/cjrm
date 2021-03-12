630c9f3579ddd3fd537c5551ed71a4f25e395371
Galileu Rodrigues <rlgalileu@gmail.com>
2021-03-12 00:57:44 -0300

    Aulas (O evento submit, Testanto padroes de regex) Finalizadas

---
+++ etapa06/testando_padroes_de_regex/app.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('.signup-form')
+// const usernameInput = document.querySelector('#username')
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+
+  console.log(event.target.username.value)
+})
+
+const username = 'galileuER'
+const pattern = /^[a-z]{6,}$/
+
+const isAMatch = pattern.test(username)
+
+if (isAMatch) {
+  console.log('o teste da regex passou =)')
+} else {
+  console.log('o teste da regex não passou =(')
+}
+
+// const result = username.search(parent)
+
+// console.log(result)
