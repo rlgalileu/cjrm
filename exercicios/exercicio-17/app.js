4f9188a1566efb1a6d53ca63c4e49f0fd8efaba2
Galileu Rodrigues <rlgalileu@gmail.com>
2021-03-12 01:42:27 -0300

    Exercicio 17 - Finalizado

--- exercicios/exercicio-17/app.js
+++ exercicios/exercicio-17/app.js
@@ -4,6 +4,36 @@
   - No envio do form, faça com que a página não seja recarregada.
 */
 
+const form = document.querySelector('form')
+
+const clearInput = () => {
+  input.value = ''
+  input.focus()
+}
+
+const logMessage = message => {
+  console.log(message)
+  clearInput()
+}
+
+const handleSubmit = event => {
+  event.preventDefault()
+
+  const input = event.target.input
+  const regex = /[a-zA-Z0-9]{7,11}/
+
+  const isAValidValue = regex.test(input.value)
+
+  if (isAValidValue) {
+   logMessage('O valor inserido no input é válido =)')
+    return
+  }
+
+ logMessage('Valor inválido =(')
+}
+
+form.addEventListener('submit',handleSubmit)
+
 /*
   02
 
@@ -19,6 +49,13 @@
   - Exiba no console o boolean no qual este teste resulta.
 */
 
+const p = document.querySelector('p')
+const regex = /documentation/
+const result = regex.test(p.textContent)
+
+console.log(result)
+
+
 /*
   04
 
@@ -28,6 +65,11 @@
 */
 
 const B99message = 'E o Terry Crews faz tudo, inclusive tocar a abertura de B99 na flauta'
+const B99Regex = /[A-Z0-9]{3}/
+
+const B99Result = B99Regex.test(B99message)
+
+console.log(B99Result)
 
 /*
   05
@@ -36,7 +78,7 @@ const B99message = 'E o Terry Crews faz tudo, inclusive tocar a abertura de B99 
     resultado do teste entre a regex e a string exibido no console seja true.
 */
 
-const word = 'O que a NASA fotografou no dia do seu aniversário?'
+const word = 'NASA'
 const NASARegex = /^[A-Z]{4}$/
 const NASAResult = NASARegex.test(word)
 
@@ -45,21 +87,23 @@ console.log(NASAResult)
 /*
   06
 
-  - No envio do form, se o valor inserido no input conter, no mínimo, 7  
+  - No envio do form, se o valor inserido no input conter, no mínimo, 7
     caracteres, que podem ser quaisquer caracteres, exiba no console a mensagem
     "O valor inserido no input é válido =)";
   - Caso contrário, exiba "Valor inválido =(" no console.
-  
+
   Exemplos:
     - "a[b@X7c" é um valor válido, pois contém 7 caracteres;
     - "jozeti" não é um valor válido, pois contém 6 caracteres.
 */
 
+
+
 /*
   07
 
-  - Agora, no envio do form, faça com que o valor permitido contenha de 7 a 11 
-    caracteres mas não contenha caracteres especiais. Apenas letras maiúsculas  
+  - Agora, no envio do form, faça com que o valor permitido contenha de 7 a 11
+    caracteres mas não contenha caracteres especiais. Apenas letras maiúsculas
     ou minúsculas e números serão permitidos.
 
   Exemplos:
